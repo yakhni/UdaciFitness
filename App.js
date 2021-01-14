981ded350aa0e56fe88af6b98067954cc6cbe41e
Farhan Ul Haq <farhan@openvpn.net>
2021-01-13 16:41:01 -0800

    Create history component

--- App.js
+++ App.js
@@ -5,12 +5,13 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
+import History from './components/History'
 
 export default function App() {
   return (
     <Provider store={createStore(reducer, middleware)}>
       <View style={{flex: 1}}>
-        <AddEntry />
+        <History />
       </View>
     </Provider>
   )
