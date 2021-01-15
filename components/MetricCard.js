60cb8887c339921f612039d39ff32c161cfe1b95
Farhan Ul Haq <farhan@openvpn.net>
2021-01-14 18:19:42 -0800

    Comment out DateHeader from MetricCard

--- components/MetricCard.js
+++ components/MetricCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
-import DateHeader from './DateHeader'
+// import DateHeader from './DateHeader'
 import { getMetricMetaInfo } from '../utils/helpers'
 import { gray } from '../utils/colors'
 
-export default function MetricCard ({ date, metrics }) {
+export default function MetricCard ({ metrics }) {
   return (
     <View>
-      {date && <DateHeader date={date} />}
+      {/* {date && <DateHeader date={date} />} */}
       {Object.keys(metrics).map((metric) => {
         const { getIcon, displayName, unit, backgroundColor } = getMetricMetaInfo(metric)
         return (
