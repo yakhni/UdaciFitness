c0c943823ca1831641c3111c7ddee37a8865030c
Farhan Ul Haq <farhan@openvpn.net>
2021-01-15 14:44:51 -0800

    Set EntryDetail title to selected day

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class EntryDetail extends Component {
+
+  componentDidMount(){
+    this.setTitle(this.props.route.params.entryId);
+  }
+
+  setTitle = (entryId) => {
+    if (!entryId) return;
+    const year = entryId.slice(0, 4)
+    const month = entryId.slice(5, 7)
+    const day = entryId.slice(8)
+
+    this.props.navigation.setOptions({
+        title: `${month}/${day}/${year}`
+    });
+  };
+
   render() {
-    console.log('EntryDetail props: ', this.props)
     return (
       <View>
         <Text>Entry Detail - {JSON.stringify(this.props.route.params.entryId)}</Text>
